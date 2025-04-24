654113d902610fb1a3a5e83f8901714229713fed
Sanslogic09 <trashdeity@mail.ru>
2025-04-24 18:27:35 +0200

    trial commit

--- BunkerGame.Frontend/Program.cs
+++ BunkerGame.Frontend/Program.cs
@@ -14,7 +14,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-///todo
+//hello it s nice to meet you
 
 app.UseHttpsRedirection();
 
