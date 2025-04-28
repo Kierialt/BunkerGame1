81ddf3ea372b88d77f442288e2c6875672cbd295
Kierialt <holyangel.27@mail.ru>
2025-04-28 12:22:50 +0200

    for api

--- BunkerGame.Backend/Responses/ApiResponse.cs
+++ BunkerGame.Backend/Responses/ApiResponse.cs
@@ -1,6 +1,17 @@
 namespace BunkerGame.Backend.Responses;
 
-public class ApiResponse
+public class ApiResponse<T>
 {
-    
+    public bool Success { get; set; }
+    public string Message { get; set; } = string.Empty;
+    public T? Data { get; set; }
+
+    public ApiResponse() { }
+
+    public ApiResponse(bool success, string message, T? data = default)
+    {
+        Success = success;
+        Message = message;
+        Data = data;
+    }
 }
