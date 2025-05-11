1b0567db2373485563923e4e07e403fc6dafaece
Kierialt <holyangel.27@mail.ru>
2025-05-11 16:58:58 +0200

    commit

--- BunkerGame.Backend/Models/User.cs
+++ BunkerGame.Backend/Models/User.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BunkerGame.Backend.Models;
 
 public class User
 {
     public int Id { get; set; }
+    [Required]
+    [MaxLength(255)] // Ограничение длины строки
+    [EmailAddress]   // Проверка на валидный e-mail (необязательно, но полезно)
     public string Email { get; set; } = null!;
     public byte[] PasswordHash { get; set; } = null!;
     public byte[] PasswordSalt { get; set; } = null!;
