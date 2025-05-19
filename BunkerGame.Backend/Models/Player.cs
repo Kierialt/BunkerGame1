3d106aff0f1c8c3fc705ddb82b7f301fa7db284b
Kierialt <holyangel.27@mail.ru>
2025-05-19 20:58:59 +0200

    new attributes

--- BunkerGame.Backend/Models/Player.cs
+++ BunkerGame.Backend/Models/Player.cs
@@ -8,7 +8,16 @@ public class Player
     public string Orientation { get; set; } = default!;
  
     public string Name { get; set; } = default!;
-
+    
+    public string Hobby { get; set; } = default!;
+    
+    public string Phobia { get; set; } = default!;
+    
+    public string Luggage { get; set; } = default!;
+    
+    public string AdditionalInformation { get; set; } = default!;
+    
+    public string BodyType { get; set; } = default!;
     public string Health { get; set; } = default!;
     
     
