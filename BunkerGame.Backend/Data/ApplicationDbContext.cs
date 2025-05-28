ee2cf9b5aa17fd7270abf6fbad41ab795f87850c
Kierialt <holyangel.27@mail.ru>
2025-05-28 16:14:59 +0200

    Remove unused parameterless constructor in ApplicationDbContext

--- BunkerGame.Backend/Data/ApplicationDbContext.cs
+++ BunkerGame.Backend/Data/ApplicationDbContext.cs
@@ -9,12 +9,11 @@ namespace BunkerGame.Backend.Data;
             : base(options)
         {
         }
-
-        public ApplicationDbContext()
-        {
-        }
+        
 
         public DbSet<User> Users { get; set; } = null!;
         public DbSet<Player> Players { get; set; } = null!;
+        
+        
     }
 
