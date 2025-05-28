78daac3f79f87e8da902a1e77f9d4d790cf15bf0
Kierialt <holyangel.27@mail.ru>
2025-05-28 22:46:17 +0200

    Add Nickname property to LoginDto
    ##"Introduce a new nullable property, Nickname, in the LoginDto class. This change allows optional inclusion of a nickname during login-related operations. Defaults for other properties are also updated for consistency."

--- BunkerGame.Backend/DTOs/LoginDto.cs
+++ BunkerGame.Backend/DTOs/LoginDto.cs
@@ -2,6 +2,7 @@ namespace BunkerGame.Backend.DTOs;
 
 public class LoginDto
 {
-    public string Email { get; set; } = null!;
-    public string Password { get; set; } = null!;
+    public string Email { get; set; } = default!;
+    public string Password { get; set; } = default!;
+    public string? Nickname { get; set; } = default!;
 }
