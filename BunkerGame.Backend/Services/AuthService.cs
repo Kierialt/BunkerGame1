d79b4ef5cda038d87daa718eb6602d275b0c853c
Kierialt <holyangel.27@mail.ru>
2025-05-28 22:48:03 +0200

    Add support for Nickname in auth service
    ##"Updated the RegisterAsync method to save the user's nickname during registration. Modified the LoginAsync method to return both email and nickname instead of just email, improving the data provided upon user login."

--- BunkerGame.Backend/Services/AuthService.cs
+++ BunkerGame.Backend/Services/AuthService.cs
@@ -14,6 +14,7 @@ public class AuthService : IAuthService
     {
         _context = context;
     }
+    
 
     public async Task<bool> RegisterAsync(RegisterDto request)
     {
@@ -23,6 +24,7 @@ public class AuthService : IAuthService
         using var hmac = new HMACSHA512();
         var user = new User
         {
+            Nickname = request.Nickname ?? string.Empty,
             Email = request.Email,
             PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(request.Password)),
             PasswordSalt = hmac.Key
@@ -33,18 +35,22 @@ public class AuthService : IAuthService
         return true;
     }
 
-    public async Task<string?> LoginAsync(LoginDto request)
+    public async Task<(string Email, string Nickname)?> LoginAsync(LoginDto request)
     {
         var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == request.Email);
         if (user == null) return null;
 
         using var hmac = new HMACSHA512(user.PasswordSalt);
         var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(request.Password));
-
+        
         if (!computedHash.SequenceEqual(user.PasswordHash))
             return null;
-
+        
+        //TODO!!!!
+        //var nick = (Encoding.UTF8.GetBytes(request.Nickname));
+        
         // Пока что без JWT, просто вернем Email
-        return user.Email;
+        return (user.Email, user.Nickname); 
+        
     }
 }
