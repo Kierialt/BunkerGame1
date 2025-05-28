1d92a1764ced4ac98952f99ff138ae2c701d2751
Kierialt <holyangel.27@mail.ru>
2025-05-28 16:43:21 +0200

    Remove parameterless constructor from ApiResponse class

--- BunkerGame.Backend/Responses/ApiResponse.cs
+++ BunkerGame.Backend/Responses/ApiResponse.cs
@@ -6,7 +6,6 @@ public class ApiResponse<T>
     public string Message { get; set; } = string.Empty;
     public T? Data { get; set; }
     
-    public ApiResponse() { }
 
     public ApiResponse(bool success, string message, T? data = default)
     {
