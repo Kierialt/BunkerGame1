1e9c82e264e01fa25821a22e5baad1f37170605a
Kierialt <holyangel.27@mail.ru>
2025-06-22 19:21:26 +0200

    update data for voting etc.

--- BunkerGame.Backend/Data/ApplicationDbContext.cs
+++ BunkerGame.Backend/Data/ApplicationDbContext.cs
@@ -13,6 +13,10 @@ namespace BunkerGame.Backend.Data;
         
         public DbSet<User> Users { get; set; } = null!;
         public DbSet<Player> Players { get; set; } = null!;
+        public DbSet<GameRoom> GameRooms { get; set; } = null!;
+        public DbSet<RoomPlayer> RoomPlayers { get; set; } = null!;
+        public DbSet<VotingRound> VotingRounds { get; set; } = null!;
+        public DbSet<Vote> Votes { get; set; } = null!;
         
         
     }
