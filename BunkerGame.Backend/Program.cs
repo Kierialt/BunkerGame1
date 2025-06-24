e40b613acffb93acd8006c2ccd55abafe7db1970
Kierialt <holyangel.27@mail.ru>
2025-06-24 16:08:09 +0200

    only english

--- BunkerGame.Backend/Program.cs
+++ BunkerGame.Backend/Program.cs
@@ -28,19 +28,19 @@ builder.Services.AddScoped<RoomService>();
 builder.Services.AddScoped<VotingService>();
 
 
-// Получаем строку подключения из переменной среды или из appsettings.Production/Development.json
+// Get the connection string from environment variable or from appsettings.Production/Development.json
 string dbPath = Path.Combine(AppContext.BaseDirectory, builder.Configuration.GetValue<string>("DbPath") ?? throw new InvalidOperationException());
 string connectionString = $"Data Source={dbPath}";
 
-// Настраиваем контекст
+// Configure context
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(connectionString));
 
 
-// Регистрируем сервис авторизации
+// Register authorization service
 builder.Services.AddScoped<IAuthService, AuthService>();
 
-//Регистрация сервиса бэкапа
+// Register backup service
 builder.Services.AddHostedService<BackupService>();
 
 
